2485d8929e088523b41bebffd0e095a4e05ce7fc
jleandromorais <leandromorais12345@outlook.com>
2024-10-20 15:08:08 -0300

    Adicionei o menu,o json,as bibliotecas necessarias

--- Crud.py
+++ Crud.py
@@ -1,2 +1,29 @@
-print("Ola mundo");
-print("Primeiro  commit")
+import os 
+import time
+import json
+denuncias=[]
+
+def salvar_dados():
+    with open('usuarios.json', 'w') as file:
+        json.dump(denuncias, file)
+    print("Dados salvos com sucesso!")
+
+def apresentacao():
+    print("                                 ") 
+    print("█▀█ █▀█ █   █ █▀▀ █ ▄▀█   █▀▀ █ █ █ █ █")
+    print("█▀▀ █▄█ █▄▄ █ █▄▄ █ █▀█   █▄▄ █ ▀▄▀ █ █▄▄")
+    print("Peça um pizza=Denunciar incidentes ,coca=probelmas e atividades suspeitas=pizza e coca")
+    print("Faça sua denuncia com a gente de forma segura!")
+    time.sleep(3)
+    os.system("cls")
+    
+
+while True:
+    apresentacao()
+    print("+=============Menu Krust burger===================+")
+    print("Ola Bem vindo ao krust Burger,qual seria seu pedido ?")
+    print("1.Peça um pizza")
+    print("2.coca")
+    print("3. Coca e pizza")
+    escolha=int(input("Escolha um para continuar:"))
+    
