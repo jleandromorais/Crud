ca326b66a7094bf65ee342e8ec734009c6af03f2
kayky <kaykydiasdi@gmail.com>
2024-10-24 14:17:37 -0300

    +json p/ categorias de denuncias

--- Crud.py
+++ Crud.py
@@ -1,43 +1,57 @@
 import os 
 import time
 import json
-denuncias=[]
+
+denuncias = [
+    {'Categoria':'',
+     'Data': '',
+     'Descrição': '',
+     'Protocolo': ''}
+]
+
+usuarios_adm = [
+    {'Nome':'',
+     'Idade':'',
+     'Email':'',
+     'Telefone':'',}
+]
+
+categorias_denuncias = [
+    {'Categorias':['Roubo', 'Furto', 'Assédio', 'Agressão Física', 'Extorsão']}
+]
+
+with open('categorias_denuncias.json', 'w', encoding='utf-8') as arquivo:
+    json.dump(categorias_denuncias, arquivo, ensure_ascii=False, indent=4)
 
 
+
+senha_adm = 123456
 
 def apresentacao():
     print("                                 ") 
     print("█▀█ █▀█ █   █ █▀▀ █ ▄▀█   █▀▀ █ █ █ █ █")
     print("█▀▀ █▄█ █▄▄ █ █▄▄ █ █▀█   █▄▄ █ ▀▄▀ █ █▄▄")
-    print("pedio=faça sua denuncia")
     print("Faça sua denuncia com a gente de forma segura!")
-    time.sleep(3)
+    print("                                 ")
+    time.sleep(1)
     os.system("cls")
-    
-    
+
 #Função do usuario pedir foto,faça uma função que peça ao usuario se ele quer pedir uma foto ou vide
-
-
 
 def main():
     apresentacao()
-    while True: #QUEM QUISER OPINAR SOBRE O MENU SERIA BOM!POIIS SO ADICIONEI ALGO DIFERENTE NO 6
-        print("="*20, "Denúncias", "="*20)
-        print("[1] Área de Administrador")
+    while True:
+        print("\n[1] Área de Administrador")
         print("[2] Denúncia Anônima")
         print("[3] Denúncias em Andamento")
         print("[4] Sair")
-        escolha = int(input("Escolha um para continuar: "))
+        escolha = int(input("\nEscolha um para continuar: "))
 
 def menu_denuncia():
-    print("="*20, "Denúncias", "="*20)
-    print("Ola Bem vindo ao krust Burger,o que voce ira querer? ?")
-    print("1.Fazer pedido")
-    print("2.Ver os pedidos")
-    print("3.Atualizar pedido")
-    print("4.Excluir pedido")
-    print("5.Rastrear pedido e mostrar statuss")
-    print("7.Sair")
+    print("="*20, "Denúncia Anônima", "="*20)
+    print("[1] Realizar Denúncia Anônima")
+    print("[2] Denúncias em Andamento")
+    print("[3] Sair")
     escolha = int(input("Escolha um para continuar:"))
 
 
