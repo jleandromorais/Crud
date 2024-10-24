5c70de5dea9d2753a4ecbde86593de059d6b3e91
kayky <kaykydiasdi@gmail.com>
2024-10-24 14:24:26 -0300

    minor changes in dictos

--- Crud.py
+++ Crud.py
@@ -2,24 +2,22 @@ import os
 import time
 import json
 
-denuncias = [
-    {'Categoria':'',
+denuncias ={
+     'Categoria':'',
      'Data': '',
      'Descrição': '',
      'Protocolo': ''}
-]
 
-usuarios_adm = [
-    {'Nome':'',
+usuarios_adm ={
+     'Nome':'',
      'Idade':'',
      'Email':'',
      'Telefone':'',
      'Senha':''}
-]
 
-categorias_denuncias = [
-    {'Categorias':['Roubo', 'Furto', 'Assédio', 'Agressão Física', 'Extorsão']}
-]
+categorias_denuncias = {
+    'Categorias':['Roubo', 'Furto', 'Assédio', 'Agressão Física', 'Extorsão']
+}
 
 with open('categorias_denuncias.json', 'w', encoding='utf-8') as arquivo:
     json.dump(categorias_denuncias, arquivo, ensure_ascii=False, indent=4)
