6a118b12d4d1a11fbb5abd2a4307da5b3d6dce71
kayky <kaykydiasdi@gmail.com>
2024-10-24 13:23:14 -0300

    Menu inicial atualizado

--- Crud.py
+++ Crud.py
@@ -22,17 +22,30 @@ def apresentacao():
 def main():
     apresentacao()
     while True: #QUEM QUISER OPINAR SOBRE O MENU SERIA BOM!POIIS SO ADICIONEI ALGO DIFERENTE NO 6
-        
-        print("+=============Menu Krust burger===================+")
-        print("Ola Bem vindo ao krust Burger,o que voce ira querer? ?")
-        print("1.Fazer pedido")
-        print("2.Ver os pedidos")
-        print("3.Atualizar pedido")
-        print("4.Excluir pedido")
-        print("5.Rastrear pedido e mostrar statuss")
-        print("7.Sair")
-        escolha=int(input("Escolha um para continuar:"))
+        print("="*20, "Denúncias", "="*20)
+        print("[1] Área de Administrador")
+        print("[2] Denúncia Anônima")
+        print("[3] Denúncias em Andamento")
+        print("[4] Sair")
+        escolha = int(input("Escolha um para continuar:"))
+
+def menu_denuncia():
+    print("="*20, "Denúncias", "="*20)
+    print("Ola Bem vindo ao krust Burger,o que voce ira querer? ?")
+    print("1.Fazer pedido")
+    print("2.Ver os pedidos")
+    print("3.Atualizar pedido")
+    print("4.Excluir pedido")
+    print("5.Rastrear pedido e mostrar statuss")
+    print("7.Sair")
+    escolha = int(input("Escolha um para continuar:"))
 
 
 if __name__=="__main__":
       main()
+
+#CRUD 1
+
+#CRUD 2
+
+#CRUD 3
