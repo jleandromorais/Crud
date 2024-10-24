a5925519ede91bdbafffef143994ec3f23e40c45
kayky <kaykydiasdi@gmail.com>
2024-10-24 15:01:07 -0300

    major changes in menus

--- Crud.py
+++ Crud.py
@@ -38,17 +38,36 @@ def main():
     while True:
         print("\n[1] Área de Administrador")
         print("[2] Denúncia Anônima")
-        print("[3] Denúncias em Andamento")
-        print("[4] Sair")
+        print("[3] Sair")
         escolha = int(input("\nEscolha um para continuar: "))
 
 def menu_denuncia():
     print("="*20, "Denúncia Anônima", "="*20)
     print("[1] Realizar Denúncia Anônima")
-    print("[2] Denúncias em Andamento")
+    print("[2] Busca por Denúncia")
     print("[3] Sair")
     escolha = int(input("Escolha um para continuar:"))
 
+def menu_adm():
+    print("="*20, "Área de Administrador", "="*20)
+    print("[1] Cadastrar Administrador")
+    print("[2] Listar Administradores")
+    print("[3] Atualizar Administrador")
+    print("[4] Remover Administrador")
+    print("[5] Listar Todas as Denúncias")
+    print("[6] Atualizar Progresso de Denúncia")
+    print("[7] Edição de Categorias")
+    print("[8] Sair")
+    escolha = int(input("Escolha um para continuar:"))
+
+def menu_categorias():
+    print("="*20, "Área de Administrador", "="*20)
+    print("[1] Criar Categoria de Denúncia")
+    print("[2] Listar Categorias")
+    print("[3] Editar Categoria")
+    print("[4] Remover Categoria")
+    print("[5] Sair")
+    escolha = int(input("Escolha um para continuar:"))
 
 if __name__=="__main__":
       main()
